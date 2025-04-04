740f830a426784319998e26320117385a9262442
giovanaa1909 <rm95548@estudante.fieb.edu.br>
2025-04-04 09:55:27 -0300

    Componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import {useState} from "react";
+import poke from './assets/pokeapi_256.png'
 
 function App () {
 
-  const [nome, setNome] = useState("Gigi")
-  
   // var nome = "Giovana";
 
-  const Formulario = () => {
+  const Formulario = (props) => {
+
+    const [nome, setNome] = useState("Gigi")
+
   return (
     <div>
+
       <input 
         className="nome"
         name = "nome"
         onChange={(e)=>{setNome(e.target.value)}}
-        placeholder="Digite um nome..."
+        placeholder={props.sombra != null ? props.sombra : "Texto padrão"}
         type="text" />
+
       <button 
         className="botão"
         onClick={()=>{alert(nome)}} >
         CLIQUE AQUI
       </button>
+
     </div>
     )
   }
@@ -27,9 +32,16 @@ function App () {
   return (
     <div>
       <h3>Pizzaria 2F</h3>
+      <p>Texto do paragrafo</p>
+      <img src={poke} style={{width:200, height:100}}/>
+      <img src={poke} style={{width:200, height:100}}/>
+      <Formulario sombra="Digite seu nome..." />
+      <Formulario sombra="Digite seu email.."/>
+      <Formulario sombra="(99)90000-0000"/>
       <Formulario />
       <Formulario />
       <Formulario />
+
     </div>
   )
 }
