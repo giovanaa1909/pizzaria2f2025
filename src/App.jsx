79104d950c1664550e2a2b20f3062b773e464ce9
giovanaa1909 <rm95548@estudante.fieb.edu.br>
2025-09-26 08:29:05 -0300

    Cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-
-import Produtos from "./pages/Produtos"
+import Produtos from "./pages/produtos"
+import CadastroProduto from "./pages/produtos/cadastro"
 
 const App = () => {
 
@@ -7,9 +7,9 @@ const App = () => {
   return (
     <>
     <h1>Projeto Pizzaria</h1>
+    <CadastroProduto />
     <Produtos/>
     </>
-
   )
 }
 
